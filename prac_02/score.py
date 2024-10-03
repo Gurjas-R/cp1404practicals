59e4e93d8fa9808c2589614fbd784f77836f7e74
Gurjy <qsilver199@gmail.com>
2024-10-03 17:16:29 +1000

    Added the SRP functions for the decision structure and the random score generation.

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,17 +2,36 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(graded_score(score))
+
+    score = generate_random_score(score)
+    print(score)
+    print(graded_score(score))
+
+
+def graded_score(score):
+    """Determine grade for score  """
+    if score < 0:
+        return "Invalid score"
+    elif score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def generate_random_score(score):
+    """"Generate a random integer for score"""
+    score = random.randint(0, 100)
+    return score
+
+
+main()
