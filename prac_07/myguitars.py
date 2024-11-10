8005c2861c36759dadca41a12c554e204365ac0f
Gurjy <qsilver199@gmail.com>
2024-11-10 21:36:11 +1000

    Add User Input Function/File Write to myguitars.py

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -15,6 +15,8 @@ def main():
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    add_new_guitars(guitars)
+    save_file(FILENAME, guitars)
 
 
 def read_file(filename, guitars):
@@ -24,6 +26,24 @@ def read_file(filename, guitars):
             parts = line.strip().split(',')
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
+
+
+def add_new_guitars(guitars):
+    """Ask the user to input new guitars."""
+    name = input("Guitar name: ")
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    new_guitar = Guitar(name, year, cost)
+    guitars.append(new_guitar)
+    print(f"{new_guitar} added.")
+
+
+def save_file(filename, guitars):
+    """Save the guitars list to the file."""
+    with open(filename, "w", encoding="utf-8-sig") as out_file:
+        for guitar in guitars:
+            out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+    print("Guitars saved to file.")
 
 
 main()
